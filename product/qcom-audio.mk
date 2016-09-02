dff422e9e15fe414474ee79cb38bb7163b69d62c
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-09-02 18:14:04 +0200

    msm8916-common: Import CAF audio configs
    * CAF Package version: LA.BR.1.2.9-00810-8x16.0
    
    Change-Id: I2a1d0795d21d31e2a39b4b64ed64bd199314e290
    
    Conflicts:
    	BoardConfigCommon.mk
    	audio/audio_effects.conf
    	audio/sound_trigger_mixer_paths.xml
    	msm8916.mk
    	proprietary-files.txt

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -8,6 +8,15 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
 
+# XML Audio configuration files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/system/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/system/etc/audio_policy_volumes.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/system/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml
