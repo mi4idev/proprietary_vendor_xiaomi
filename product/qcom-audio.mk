d6bdefc96c14dc00121c554ad7a6f6d1d97cd60f
rohan <purohit.rohan@gmail.com>
2016-10-08 23:52:04 +0530

    Update audio HAL from caf
    * Revision LA.BR.1.2.9-01010-8x16.0
    
    Change-Id: If1509089e4b55ab0320ab9806ac4c5aa01ba28c0

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -3,9 +3,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/init.qcom.audio.sh:system/etc/init.qcom.audio.sh \
     $(LOCAL_PATH)/audio/mixer_paths_wcd9306.xml:system/etc/mixer_paths_wcd9306.xml \
-    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
+    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_wcd9306.xml:system/etc/sound_trigger_mixer_paths_wcd9306.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
 
 # XML Audio configuration files
