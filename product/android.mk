191b5984bb63f9b559f626461771de6bb6b21d15
rohan <purohit.rohan@gmail.com>
2016-10-10 19:13:46 +0530

    Add fs_config support for custom binary.
    Build custom target binary to override the default
    permission.

--- product/android.mk
+++ product/android.mk
@@ -1,3 +1,7 @@
 # First api level, device has been commercially launched
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.first_api_level=22
+
+# For android_filesystem_config.h
+PRODUCT_PACKAGES += \
+    fs_config_files
